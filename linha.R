18be8ac53964ac81d65c930875696f64022f4bad
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-02 23:42:33 +0100

    Create linha.R

---
+++ linha.R
@@ -0,0 +1,14 @@
+library(dplyr)
+library(ggplot2)
+library(hrbrthemes)
+
+data <- read.table("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/3_TwoNumOrdered.csv", header=T)
+data$date <- as.Date(data$date)
+
+data %>%
+  ggplot( aes(x=date, y=value)) +
+  geom_line(color="#69b3a2") +
+  ggtitle("Evolução do preço do Bitcoin") +
+  xlab("Ano") +
+  ylab("Preço Bitcoin ($)") +
+  theme_ipsum()
