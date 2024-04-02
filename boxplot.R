22b15e28dba4e3f2da7d6bb4084efa300440a715
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-02 23:38:13 +0100

    Create boxplot.R

---
+++ boxplot.R
@@ -0,0 +1,16 @@
+library(dplyr)
+library(ggplot2)
+library(hrbrthemes)
+
+mpg %>%
+  mutate(Tipo = ifelse(class == "subcompact", "Destacado", "Normal")) %>%
+  ggplot(aes(x = class, y = hwy, fill = Tipo)) + 
+  geom_boxplot() +
+  scale_fill_manual(values = c("#69b3a2", "#e9ecef")) +
+  scale_alpha_manual(values = c(1, 0.1)) +
+  ggtitle("Consumo de combustível por tipo de veículo") +
+  xlab("Categorias") +
+  ylab("Milhas por Galão") +
+  theme(legend.position = "right") + 
+  theme_ipsum() +
+  guides(fill = guide_legend(title = NULL))
