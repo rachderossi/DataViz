df237e0dcdfddf8935b2dda76fc6796b42e07a53
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-03 01:28:55 +0100

    Update linha.py

--- linha.py
+++ linha.py
@@ -1,18 +1,19 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-url = "https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/3_TwoNumOrdered.csv"
-data = pd.read_csv(url)
+data = pd.read_csv(
+    "https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/3_TwoNumOrdered.csv", 
+    delim_whitespace=True
+)
 
-data[['date', 'value']] = data['date value'].str.split(expand=True)
-data.drop(columns=['date value'], inplace=True)
-data['date'] = pd.to_datetime(data['date'])
+data["date"] = pd.to_datetime(data["date"])
 
+date = data["date"]
+value = data["value"]
 
 plt.figure(figsize=(10, 6))
-plt.plot(data['date'], data['value'], color='#69b3a2')
+plt.fill_between(data['date'], data['value'], color="#69b3a2", alpha=0.5, zorder=1)
 plt.title("Evolução do preço do Bitcoin")
 plt.xlabel("Ano")
 plt.ylabel("Preço Bitcoin ($)")
-plt.grid(True)
 plt.show()
