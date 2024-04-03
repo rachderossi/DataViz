9834136484e7f5133d230692e7a02713ecc2e4fe
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-03 01:19:17 +0100

    Create linha.py

---
+++ linha.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+url = "https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/3_TwoNumOrdered.csv"
+data = pd.read_csv(url)
+
+data[['date', 'value']] = data['date value'].str.split(expand=True)
+data.drop(columns=['date value'], inplace=True)
+data['date'] = pd.to_datetime(data['date'])
+
+
+plt.figure(figsize=(10, 6))
+plt.plot(data['date'], data['value'], color='#69b3a2')
+plt.title("Evolução do preço do Bitcoin")
+plt.xlabel("Ano")
+plt.ylabel("Preço Bitcoin ($)")
+plt.grid(True)
+plt.show()
