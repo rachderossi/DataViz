b7cb2f9fd1a3f3fa1d1f7df9b5aa7248f96e32b9
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-13 01:15:59 +0100

    Update area.R

--- area.R
+++ area.R
@@ -4,12 +4,12 @@ library(hrbrthemes)
 
 data <- read.table("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/3_TwoNumOrdered.csv", header=T)
 data$date <- as.Date(data$date)
-
+  
 data %>%
-  ggplot( aes(x=date, y=value)) +
-  geom_area(fill="#69b3a2", alpha=0.5) +
-  geom_line(color="#69b3a2") +
-  ggtitle("Evolução do preço do Bitcoin") +
-  xlab("Ano") +
-  ylab("Preço Bitcoin ($)") +
-  theme_ipsum()
+    ggplot( aes(x=date, y=value)) +
+    geom_area(fill="#69b3a2", alpha=0.5) +
+    geom_line(color="#69b3a2") +
+    ggtitle("Evolução do preço do Bitcoin") +
+    xlab("ano") +
+    ylab("preço do Bitcoin ($)") +
+    theme_ipsum()
