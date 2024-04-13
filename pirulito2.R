8913dbb8f42f92b286b3fee469604725069631f0
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-13 01:41:52 +0100

    Create pirulito2.R

---
+++ pirulito2.R
@@ -0,0 +1,24 @@
+library(dplyr)
+library(ggplot2)
+library(hrbrthemes)
+
+ data <- read.table("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/7_OneCatOneNum.csv", header=TRUE, sep=",")
+  
+  data %>%
+    filter(!is.na(Value)) %>%
+    arrange(Value) %>%
+    tail(20) %>%
+    mutate(Country=factor(Country, Country)) %>%
+    ggplot( aes(x=Country, y=Value) ) +
+    geom_segment( aes(x=Country ,xend=Country, y=0, yend=Value), color="grey") +
+    geom_point(size=3, color="#69b3a2") +
+    coord_flip() +
+    theme_ipsum() +
+    theme(
+      panel.grid.minor.y = element_blank(),
+      panel.grid.major.y = element_blank(),
+      legend.position="none",
+      plot.title = element_text(hjust = 0.5, size = 15)) +
+    ggtitle("Quantidade de armas exportadas pelos 20 maiores exportadores em 2017") +
+    xlab("países") +
+    ylab("quantidade de armas") 
