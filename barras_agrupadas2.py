22d673a7ff60439a0b50f4cc30161488f6c7ead5
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-13 18:42:53 +0100

    Update barras_agrupadas2.py

--- barras_agrupadas2.py
+++ barras_agrupadas2.py
@@ -1,27 +1,21 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-dados = {
-    'name': ['Anna', 'Mary', 'Peter', 'John', 'Anna', 'Mary', 'Peter', 'John', 'Anna', 'Mary', 'Peter', 'John', 'Anna', 'Mary'],
-    'year': [1950, 1950, 1950, 1950, 1960, 1960, 1960, 1960, 1970, 1970, 1970, 1970, 1980, 1980],
-    'Contagem': [100, 120, 80, 90, 110, 130, 70, 95, 105, 125, 85, 100, 95, 105]
-}
+url = "https://github.com/hadley/data-baby-names/raw/master/baby-names.csv"
+babynames = pd.read_csv(url)
 
-babynames = pd.DataFrame(dados)
+names = ['Anna', 'Mary']
+filtered_data = babynames[babynames['name'].isin(names)]
 
-data = babynames[(babynames['name'].isin(['Anna', 'Mary']))]
+selected_years = [1950, 1960, 1970, 1980]
+filtered_data = filtered_data[filtered_data['year'].isin(selected_years)]
 
-# Filtrando os anos específicos
-anos_selecionados = [1950, 1960, 1970, 1980]
-data = data[data['year'].isin(anos_selecionados)]
-
-# Agregando a contagem de frequência para cada nome e ano
-data = data.pivot_table(index='year', columns='name', values='Contagem', aggfunc='sum', fill_value=0)
+data = filtered_data.pivot_table(index='year', columns='name', values='percent', aggfunc='sum', fill_value=0)
 
 data.plot(kind='bar', stacked=False, figsize=(12, 6))
 plt.xlabel('Anos')
-plt.ylabel('Número de bebês')
-plt.title('Número de bebês chamados Anna e Mary ao longo dos anos')
+plt.ylabel('Percentual de bebês')
+plt.title('Percentual de bebês chamados Anna e Mary ao longo dos anos')
 plt.legend(title=None)
 plt.xticks(rotation=45)
 plt.tight_layout()
