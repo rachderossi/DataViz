d2b97fc82e74930adaac36fc22649c589ee79837
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-13 01:38:55 +0100

    Create linha2.R

---
+++ linha2.R
@@ -0,0 +1,18 @@
+library(dplyr)
+library(ggplot2)
+library(hrbrthemes)
+library(babynames
+
+  name <- babynames %>%
+    filter(name %in% c("Ashley", "Patricia", "Helen")) %>%
+    filter(sex == "F")
+  
+  name %>%
+    ggplot(aes(x = year, y = n, group = name, color = name)) +
+    geom_line() +
+    scale_color_manual(values = c("#6E9AF8", "#009E73", "#FFA07A"), name = NULL) +
+    ggtitle("Popularidade de nomes Americanos nos últimos 30 Anos") +
+    xlab("ano") +
+    ylab("número de bebês") +
+    theme_ipsum() +
+    guides(color = guide_legend(title.position = "left", title.hjust = 1, title = NULL)) 
