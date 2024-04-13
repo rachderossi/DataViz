818b04b184d51261f4016dffc33387480c4f8f8d
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-13 01:25:28 +0100

    Update barras.R

--- barras.R
+++ barras.R
@@ -2,7 +2,8 @@ library(dplyr)
 library(ggplot2)
 library(hrbrthemes)
 
-ggplot(mpg, aes(x = class)) + 
+mpg %>%
+ggplot(aes(x = class)) + 
     geom_bar(fill = "#6E9AF8") +  
     coord_flip() +
     xlab("classe") +
