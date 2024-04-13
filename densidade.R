1deb4768540c58df211155a75e7e52a9b4c63e90
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-13 01:27:29 +0100

    Update densidade.R

--- densidade.R
+++ densidade.R
@@ -2,13 +2,13 @@ library(dplyr)
 library(ggplot2)
 library(hrbrthemes)
 
-data <- read.table("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/1_OneNum.csv", header=TRUE)
-
-data %>%
-  filter( price<300 ) %>%
-  ggplot( aes(x=price)) +
-  geom_density(fill="#69b3a2", color="#e9ecef", alpha=0.8) +
-  ggtitle("Distribuição de preços de aluguel") +
-  xlab("preço") +
-  ylab("densidade") +
-  theme_ipsum()
+  data <- read.table("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/1_OneNum.csv", header=TRUE)
+  
+  data %>%
+      filter( price<300 ) %>%
+      ggplot( aes(x=price)) +
+      geom_density(fill="#69b3a2", color="#e9ecef", alpha=0.8) +
+      ggtitle("Distribuição de preços de aluguel") +
+      xlab("preço") +
+      ylab("densidade") +
+      theme_ipsum()
