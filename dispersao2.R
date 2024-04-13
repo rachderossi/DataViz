3e040c73280f4f8b17b9a1f829b6bc47a20cf74e
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-13 01:32:53 +0100

    Update dispersao2.R

--- dispersao2.R
+++ dispersao2.R
@@ -2,7 +2,8 @@ library(dplyr)
 library(ggplot2)
 library(hrbrthemes)
 
-  ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width, color = Species)) + 
+iris %>%
+  ggplot(aes(x = Sepal.Length, y = Sepal.Width, color = Species)) + 
     geom_point(size = 3) +
     ggtitle("Relação entre comprimento e largura das sépalas por espécie") +
     xlab("comprimento") +
