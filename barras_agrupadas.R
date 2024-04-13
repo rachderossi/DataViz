d320ae2d303e58815db6d318256049569dcee45c
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-13 01:18:04 +0100

    Update barras_agrupadas.R

--- barras_agrupadas.R
+++ barras_agrupadas.R
@@ -1,20 +1,11 @@
 library(dplyr)
 library(ggplot2)
 library(hrbrthemes)
-library(babynames)
 
-data <- babynames %>% 
-  filter(name %in% c("Anna", "Mary")) %>%
-  filter(sex=="F")
-
-data  %>% 
-  filter(year %in% c(1950, 1960, 1970, 1980, 1990, 2000)) %>%
-  mutate(year=as.factor(year)) %>%
-  ggplot( aes(x=year, y=n, fill=name)) +
-  geom_bar(stat="identity", position="dodge") +
-  scale_fill_manual(values = c("Anna" = "#6E9AF8", "Mary" = "#69b3a2")) + 
-  theme_ipsum() +
-  ylab("Número de bebês") +
-  xlab("Anos") +
-  ggtitle("Número de bebês chamados Anna e Mary ao longo dos anos") +
-  guides(fill = guide_legend(title = NULL))
+ggplot(diamonds, aes(x = cut, fill = color)) +
+    geom_bar(position = "dodge") +
+    xlab("corte") +
+    ylab("frequência") +
+    labs(fill = "cor") +
+    ggtitle("Qualidade do corte de diamante de acordo com a cor") +
+    theme_ipsum() 
