e2d9369457f435fcf78fc4759ba6b158b4625658
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-13 01:24:08 +0100

    Create boxplot3.R

---
+++ boxplot3.R
@@ -0,0 +1,12 @@
+library(dplyr)
+library(ggplot2)
+library(hrbrthemes)
+
+  ggplot(mtcars, aes(x = as.factor(cyl), y = mpg, fill = factor(am, labels = c("Automático", "Manual")))) +
+    geom_boxplot() +
+    xlab("número de cilindros") +
+    ylab("milhas por galão") +
+    ggtitle("Consumo de combustível por cilindros e tipo de transmissão") +
+    scale_fill_manual(values = c("#009E73", "#6E9AF8")) +
+    theme_ipsum() +
+    guides(fill = guide_legend(title = NULL))
