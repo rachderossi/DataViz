e65dfea4540fef179e48011a45d06dd63dcdc578
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-18 14:20:05 +0100

    Update pirulito.py

--- pirulito.py
+++ pirulito.py
@@ -1,18 +1,24 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-freq_class = mpg['class'].value_counts()
+# Criando um DataFrame de exemplo similar ao 'mpg'
+data = {
+    'class': ['compact', 'midsize', 'suv', 'midsize', 'compact', 'suv', 'suv', 'compact'],
+}
+mpg = pd.DataFrame(data)
 
-data_freq = pd.DataFrame({'class': freq_class.index, 'freq': freq_class.values})
+# Contando as ocorrências de cada classe
+class_counts = mpg['class'].value_counts().reset_index()
+class_counts.columns = ['class', 'freq']
 
-data_freq = data_freq.sort_values(by='freq')
+# Ordenando o DataFrame pela frequência em ordem ascendente
+class_counts = class_counts.sort_values(by='freq')
 
 plt.figure(figsize=(10, 6))
-plt.hlines(y=data_freq['class'], xmin=0, xmax=data_freq['freq'], color='black')
-plt.plot(data_freq['freq'], data_freq['class'], 'o', color='#6E9AF8', markersize=8)
+plt.hlines(y=class_counts['class'], xmin=0, xmax=class_counts['freq'], color='black')
+plt.plot(class_counts['freq'], class_counts['class'], 'o', color='#6E9AF8', markersize=8)
 plt.xlabel('Frequência')
 plt.ylabel('Classe')
-plt.title('Distribuição de classes de veículos')
-plt.grid(axis='x', linestyle='--', alpha=0.7)
-plt.tight_layout()
+plt.title('Distribuição das classes de veículo')
+plt.grid(True, axis='x', linestyle='--', alpha=0.7)
 plt.show()
