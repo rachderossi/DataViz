476b588b04713115dcd30ae69de7b0834bfdfd9f
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-18 14:06:42 +0100

    Update barras.R

--- barras.R
+++ barras.R
@@ -2,11 +2,12 @@ library(dplyr)
 library(ggplot2)
 library(hrbrthemes)
 
-mpg %>%
-ggplot(aes(x = class)) + 
-    geom_bar(fill = "#6E9AF8") +  
-    coord_flip() +
-    xlab("classe") +
-    ylab("frequência") +
+  mpg %>%
+    count(class) %>%
+    mutate(class = reorder(class, n)) %>%
+    ggplot(aes(x = class, y = n)) + 
+    geom_bar(fill = "#6E9AF8", stat = "identity") +  
+    xlab("Classe") +
+    ylab("Frequência") +
     ggtitle("Distribuição das classes de veículo") +
-    theme_ipsum() 
+    theme_ipsum()
