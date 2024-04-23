82afd3ffd1c65e2837d4a27a3cf2480a3caaa940
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-23 20:10:41 +0100

    Update dispersao_conectado.py

--- dispersao_conectado.py
+++ dispersao_conectado.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Substitua "caminho/para/seu/arquivo.csv" pelo caminho do arquivo CSV em sua máquina
+# df = pd.read_csv("caminho/para/seu/arquivo.csv")
+
+# Cria um DataFrame com as datas e os valores correspondentes
 df = pd.DataFrame({
     'Date': ["23-Abr", "22-Abr", "21-Abr", "20-Abr", "19-Abr", 
              "18-Abr", "17-Abr", "16-Abr", "15-Abr", "14-Abr"],
@@ -8,6 +12,7 @@ df = pd.DataFrame({
               8197.80, 8285.96, 8371.15, 8338.42, 8140.71]
 })
 
+# Cria o gráfico de dispersão conectado
 plt.figure(figsize=(10, 6))
 plt.plot(df['Date'], df['Value'], marker='o', linestyle='-', color='grey', markersize=6, label='_nolegend_')
 plt.scatter(df['Date'], df['Value'], color='#69b3a2', edgecolor='black', s=150)
