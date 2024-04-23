4caaab3f6fc0ed3e46d9bb71b6e82a415a597380
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-23 18:45:30 +0100

    Update histograma3.R

--- histograma3.R
+++ histograma3.R
@@ -1,12 +1,17 @@
 library(dplyr)
 library(ggplot2)
 library(hrbrthemes)
-  
-iris %>%
-  ggplot(aes(x = Petal.Length, fill = Species)) +
-    geom_histogram(binwidth = 0.2, alpha = 0.5, position = "identity") +  
-    xlab("comprimento da pétala") +
-    ylab("frequência") +
-    labs(fill = "espécies") +
-    ggtitle("Distribuição do comprimento da sépala por espécie de flor") +
-    theme_ipsum()
+library(readr)
+
+# Substitua "caminho/para/seu/arquivo.csv" pelo caminho do arquivo CSV em sua máquina
+# iris <- read.csv("caminho/para/seu/arquivo.csv", header = TRUE)
+
+# Criar o gráfico de histograma
+    iris %>%
+      ggplot(aes(x = Petal.Length, fill = Species)) +
+      geom_histogram(binwidth = 0.2, alpha = 0.5, position = "identity") +
+      ggtitle("Distribuição do comprimento da sépala por espécie de flor") +
+      xlab("comprimento da pétala") +
+      ylab("frequência") +
+      labs(fill = "espécies") +
+      theme_ipsum()
