4fe10bc962adcf37cdde1dd2af8944198d981776
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-23 20:33:43 +0100

    Update boxplot2.py

--- boxplot2.py
+++ boxplot2.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 # Carrega o conjunto de dados 'diamonds' fornecido pela seaborn
 diamonds = sns.load_dataset('diamonds')
 
-# Cria o gráfico de boxplot
+# Criar o gráfico de boxplot
 plt.figure(figsize=(10, 6))
 sns.boxplot(x='cut', y='price', data=diamonds, hue='color', palette='Set1')
 plt.title('Distribuição do preço dos diamantes por corte')
