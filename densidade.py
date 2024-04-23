2f880822814c15dc9a1091cb9a29fc9ff475100a
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-23 19:56:49 +0100

    Update densidade.py

--- densidade.py
+++ densidade.py
@@ -2,11 +2,19 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+# Substitua "caminho/para/seu/arquivo.csv" pelo caminho do arquivo CSV em sua máquina
+# data = pd.read_csv("caminho/para/seu/arquivo.csv")
+
+# Define a URL do arquivo CSV contendo os dados de preços de aluguel
 url = "https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/1_OneNum.csv"
+
+# Lê os dados do arquivo CSV e os carrega em um DataFrame chamado 'data'
 data = pd.read_csv(url)
 
+# Filtra os dados para incluir apenas os preços de aluguel menores que 300
 data_filtered = data[data['price'] < 300]
 
+# Cria o gráfico de densidade
 plt.figure(figsize=(10, 6))
 sns.kdeplot(data=data_filtered['price'], fill=True, color='#69b3a2', alpha=0.8)
 plt.title("Distribuição de preços de aluguel")
