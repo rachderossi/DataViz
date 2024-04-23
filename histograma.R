22703761b6fecc4e42dfab4ed46a83fef029779d
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-23 18:42:56 +0100

    Update histograma.R

--- histograma.R
+++ histograma.R
@@ -1,12 +1,18 @@
 library(dplyr)
 library(ggplot2)
 library(hrbrthemes)
+library(readr)
 
+# Substitua "caminho/para/seu/arquivo.csv" pelo caminho do arquivo CSV em sua máquina
+# data <- read.csv("caminho/para/seu/arquivo.csv", header = TRUE)
+
+# Lendo os dados de um arquivo CSV hospedado em um repositório do GitHub
   data <- read.table("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/1_OneNum.csv", header=TRUE)
-  
+
+# Criar o gráfico de histograma
   data %>%
-    filter( price<300 ) %>%
-    ggplot( aes(x=price)) +
+    filter(price<300 ) %>%
+    ggplot(aes(x=price)) +
     geom_histogram(fill="#69b3a2", color="#e9ecef", alpha=0.8) +
     ggtitle("Distribuição de preços de aluguel") +
     xlab("preço") +
