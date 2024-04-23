a8c024e5b513171bdd07e4505aa9f04b2de84d4a
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-23 18:48:00 +0100

    Update linha2.R

--- linha2.R
+++ linha2.R
@@ -2,11 +2,17 @@ library(dplyr)
 library(ggplot2)
 library(hrbrthemes)
 library(babynames)
+library(readr)
 
+# Substitua "caminho/para/seu/arquivo.csv" pelo caminho do arquivo CSV em sua máquina
+# name <- read.csv("caminho/para/seu/arquivo.csv", header = TRUE)
+
+# Filtrando o conjunto de dados babynames pelo nome e sexo de interesse
   name <- babynames %>%
     filter(name %in% c("Ashley", "Patricia", "Helen")) %>%
     filter(sex == "F")
-  
+
+# Criar o gráfico de linhas
   name %>%
     ggplot(aes(x = year, y = n, group = name, color = name)) +
     geom_line() +
@@ -14,5 +20,5 @@ library(babynames)
     ggtitle("Popularidade de nomes Americanos nos últimos 30 Anos") +
     xlab("ano") +
     ylab("número de bebês") +
-    theme_ipsum() +
-    guides(color = guide_legend(title.position = "left", title.hjust = 1, title = NULL)) 
+    guides(color = guide_legend(title.position = "left", title.hjust = 1, title = NULL)) +
+    theme_ipsum() 
