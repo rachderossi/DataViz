f87623f2f1918b2389df405f49847146c18f6d93
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-23 20:12:12 +0100

    Update histograma.py

--- histograma.py
+++ histograma.py
@@ -1,11 +1,19 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Substitua "caminho/para/seu/arquivo.csv" pelo caminho do arquivo CSV em sua máquina
+# data = pd.read_csv("caminho/para/seu/arquivo.csv")
+
+# Define a URL do arquivo CSV contendo os dados de preços de aluguel
 url = "https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/1_OneNum.csv"
+
+# Lê os dados do arquivo CSV e os carrega em um DataFrame chamado 'data'
 data = pd.read_csv(url)
 
+# Filtra os dados para incluir apenas os preços de aluguel menores que 300
 data_filtered = data[data['price'] < 300]
 
+# Cria o gráfico de histograma
 plt.figure(figsize=(10, 6))
 plt.hist(data_filtered['price'], bins=20, color='#69b3a2', edgecolor='#e9ecef', alpha=0.8)
 plt.title("Distribuição de preços de aluguel")
