df2795648b80fa61419e473d59b24debec2e7535
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-23 20:37:37 +0100

    Update histograma3.py

--- histograma3.py
+++ histograma3.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 # Carrega o conjunto de dados 'iris' fornecido pela seaborn
 iris = sns.load_dataset('iris')
 
-# Cria o gráfico de histograma
+# Criar o gráfico de histograma
 plt.figure(figsize=(10, 6))
 sns.histplot(data=iris, x='petal_length', hue='species', binwidth=0.2, alpha=0.5, multiple='stack', palette='Set1')
 plt.xlabel('Comprimento da pétala')
