a4c84413bd0a75f3d2ecd37fd6aeec7b2b3165c8
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-23 20:09:25 +0100

    Update dispersao2.py

--- dispersao2.py
+++ dispersao2.py
@@ -1,8 +1,13 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+# Substitua "caminho/para/seu/arquivo.csv" pelo caminho do arquivo CSV em sua máquina
+# iris = pd.read_csv("caminho/para/seu/arquivo.csv")
+
+# Carrega o conjunto de dados 'iris' fornecido pela seaborn
 iris = sns.load_dataset('iris')
 
+# Cria o gráfico de dispersão
 plt.figure(figsize=(10, 6))
 sns.scatterplot(data=iris, x='sepal_length', y='sepal_width', hue='species', palette='Set1', s=100)
 plt.title('Relação entre comprimento e largura das sépalas por espécie')
