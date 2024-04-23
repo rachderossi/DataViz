a04f6e878d03d7f85c801abc8e9fa90988a6c731
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-23 19:58:35 +0100

    Update densidade2.py

--- densidade2.py
+++ densidade2.py
@@ -2,10 +2,16 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
 
+# Substitua "caminho/para/seu/arquivo.csv" pelo caminho do arquivo CSV em sua máquina
+# carseats_filtered = pd.read_csv("caminho/para/seu/arquivo.csv")
+
+# Obtém o conjunto de dados 'Carseats' da biblioteca ISLR e carrega os dados
 carseats = sm.datasets.get_rdataset('Carseats', 'ISLR').data
 
+# Filtra os dados para incluir apenas os preços de carseat menores que 300
 carseats_filtered = carseats[carseats['Price'] < 300]
 
+# Cria o gráfico de densidade
 plt.figure(figsize=(10, 6))
 sns.kdeplot(data=carseats_filtered, x='Price', fill=True, label='Preço', color='#6E9AF8', alpha=0.8)
 sns.kdeplot(data=carseats_filtered, x='CompPrice', fill=True, label='Renda', color='#009E73', alpha=0.8)
