6f1f2992d526ea65458eb50efaf643aae5d25b6b
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-23 17:46:55 +0100

    Update boxplot3.R

--- boxplot3.R
+++ boxplot3.R
@@ -2,12 +2,16 @@ library(dplyr)
 library(ggplot2)
 library(hrbrthemes)
 
-mtcars %>%
-  ggplot(aes(x = as.factor(cyl), y = mpg, fill = factor(am, labels = c("Automático", "Manual")))) +
+# Substitua "caminho/para/seu/arquivo.csv" pelo caminho do arquivo CSV em sua máquina
+# mtcars <- read.csv("caminho/para/seu/arquivo.csv", header = TRUE)
+
+# Criar o gráfico de boxplot
+  mtcars %>%
+    ggplot(aes(x = as.factor(cyl), y = mpg, fill = factor(am, labels = c("Automático", "Manual")))) +
     geom_boxplot() +
+    ggtitle("Consumo de combustível por cilindros e tipo de transmissão") +
     xlab("número de cilindros") +
     ylab("milhas por galão") +
-    ggtitle("Consumo de combustível por cilindros e tipo de transmissão") +
     scale_fill_manual(values = c("#009E73", "#6E9AF8")) +
-    theme_ipsum() +
-    guides(fill = guide_legend(title = NULL))
+    guides(fill = guide_legend(title = NULL)) +
+    theme_ipsum() 
