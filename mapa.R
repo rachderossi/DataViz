fde073de0f909099d3a2685649e032b0c1d9541d
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-23 18:50:09 +0100

    Update mapa.R

--- mapa.R
+++ mapa.R
@@ -1,7 +1,13 @@
 library(dplyr)
 library(ggplot2)
 library(hrbrthemes)
+library(readr)
 
+# Substitua "caminho/para/seu/arquivo.csv" pelo caminho do arquivo CSV em sua máquina
+# data <- read.csv("caminho/para/seu/arquivo.csv", header = TRUE)
+
+# Convertendo o conjunto de dados 'mtcars' para um formato de matriz
 data <- as.matrix(mtcars)
 
+# Criar o mapa de calor
 heatmap(data, Colv = NA, Rowv = NA, scale="column", main = "Mapa de calor")
