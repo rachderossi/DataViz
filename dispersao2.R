c3542183c4b0bf961cd0d50febca3cd70e193b6f
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-23 18:39:23 +0100

    Update dispersao2.R

--- dispersao2.R
+++ dispersao2.R
@@ -1,14 +1,19 @@
 library(dplyr)
 library(ggplot2)
 library(hrbrthemes)
+library(readr)
 
-iris %>%
-  ggplot(aes(x = Sepal.Length, y = Sepal.Width, color = Species)) + 
+# Substitua "caminho/para/seu/arquivo.csv" pelo caminho do arquivo CSV em sua máquina
+# iris <- read.csv("caminho/para/seu/arquivo.csv", header = TRUE)
+
+# Criar o gráfico de dispersão
+  iris %>%
+    ggplot(aes(x = Sepal.Length, y = Sepal.Width, color = Species)) + 
     geom_point(size = 3) +
     ggtitle("Relação entre comprimento e largura das sépalas por espécie") +
     xlab("comprimento") +
     ylab("largura") +
     labs(color = "espécies") +
     theme(legend.position = "left") + 
-    theme_ipsum() +
-    guides(color = guide_legend(title = NULL))
+    guides(color = guide_legend(title = NULL)) +
+    theme_ipsum() 
