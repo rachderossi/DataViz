5a420ca4fa952923a8b8e3f13a6cb3345b40776e
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-23 20:22:00 +0100

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -1,13 +1,21 @@
 import seaborn as sns
 import pandas as pd
 from matplotlib import pyplot as plt
- 
+
+# Define a URL do arquivo CSV contendo os dados dos carros
 url = 'https://raw.githubusercontent.com/holtzy/The-Python-Graph-Gallery/master/static/data/mtcars.csv'
+
+# Lê os dados do arquivo CSV e os carrega em um DataFrame chamado 'df'
 df = pd.read_csv(url)
+
+# Define a coluna 'model' como índice do DataFrame
 df = df.set_index('model')
- 
+
+# Define uma paleta de cores personalizada com base no número de cilindros dos carros
+# Cada número de cilindros é mapeado para uma cor específica
 my_palette = dict(zip(df.cyl.unique(), ["orange","yellow","brown"]))
 row_colors = df.cyl.map(my_palette)
- 
+
+# Cria o gráfico de mapa de calor
 sns.clustermap(df, metric="correlation", method="single", cmap="Blues", standard_scale=1, row_colors=row_colors)
 plt.show()
