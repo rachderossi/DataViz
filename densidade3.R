1aeabedf15b266513e81fbaf98a3a7969a3ea638
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-23 17:58:40 +0100

    Update densidade3.R

--- densidade3.R
+++ densidade3.R
@@ -1,12 +1,17 @@
 library(dplyr)
 library(ggplot2)
 library(hrbrthemes)
+library(readr)
 
-iris %>%
-  ggplot(aes(x = Sepal.Length, fill = Species)) +
+# Substitua "caminho/para/seu/arquivo.csv" pelo caminho do arquivo CSV em sua máquina
+# iris <- read.csv("caminho/para/seu/arquivo.csv", header = TRUE)
+
+# Criar o gráfico de densidade
+  iris %>%
+    ggplot(aes(x = Sepal.Length, fill = Species)) +
     geom_density(alpha = 0.5) + 
+    ggtitle("Distribuição do comprimento da sépala por espécie de flor") +
     xlab("comprimento da sépala") +
     ylab("densidade") +
     labs(fill = "espécies") +
-    ggtitle("Distribuição do comprimento da sépala por espécie de flor") +
     theme_ipsum()
