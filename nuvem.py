71c010ee3bc6105dc2b63cea7a2cf9cad3213881
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-23 20:38:40 +0100

    Update nuvem.py

--- nuvem.py
+++ nuvem.py
@@ -11,7 +11,7 @@ frequencias = [50, 30, 40, 35, 25, 20, 45, 15, 10, 5]
 # Criar um dicionário com as palavras e suas frequências
 word_freq = {paises[i]: frequencias[i] for i in range(len(paises))}
 
-# Cria o gráfico de nuvem de palavras
+# Criar o gráfico de nuvem de palavras
 wordcloud = WordCloud(width=800, height=400, colormap='Dark2').generate_from_frequencies(word_freq)
 
 plt.figure(figsize=(10, 5))
