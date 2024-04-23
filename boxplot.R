543047773aadfc403ad286924c155ae65745ce99
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-23 17:45:13 +0100

    Update boxplot.R

--- boxplot.R
+++ boxplot.R
@@ -2,12 +2,16 @@ library(dplyr)
 library(ggplot2)
 library(hrbrthemes)
 
-mpg %>%
+# Substitua "caminho/para/seu/arquivo.csv" pelo caminho do arquivo CSV em sua máquina
+# mpg <- read.csv("caminho/para/seu/arquivo.csv", header = TRUE)
+
+# Criar o gráfico de boxplot
+  mpg %>%
     ggplot(aes(x = class, y = hwy)) + 
     geom_boxplot(fill = "#69b3a2", alpha = 0.5) +  
     ggtitle("Consumo de combustível por tipo de veículo") +
     xlab("categorias") +
     ylab("milhas por galão") +
     theme(legend.position = "left") + 
-    theme_ipsum() +
-    guides(fill = guide_legend(title = NULL))
+    guides(fill = guide_legend(title = NULL)) +
+    theme_ipsum() 
