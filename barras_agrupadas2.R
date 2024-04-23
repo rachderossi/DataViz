442ae284f717d5e7dfdcd20fd03cd93e8ba166f3
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-23 17:41:26 +0100

    Update barras_agrupadas2.R

--- barras_agrupadas2.R
+++ barras_agrupadas2.R
@@ -3,18 +3,23 @@ library(ggplot2)
 library(hrbrthemes)
 library(babynames)
 
-data <- babynames %>% 
+# Substitua "caminho/para/seu/arquivo.csv" pelo caminho do arquivo CSV em sua máquina
+# data <- read.csv("caminho/para/seu/arquivo.csv", header = TRUE)
+
+# Filtrando o conjunto de dados babynames pelo nome e sexo de interesse
+  data <- babynames %>% 
     filter(name %in% c("Anna", "Mary")) %>%
     filter(sex=="F")
-  
+
+# Criar o gráfico de barras agrupadas
   data  %>% 
     filter(year %in% c(1950, 1960, 1970, 1980, 1990, 2000)) %>%
     mutate(year=as.factor(year)) %>%
     ggplot( aes(x=year, y=n, fill=name)) +
     geom_bar(stat="identity", position="dodge") +
     scale_fill_manual(values = c("Anna" = "#6E9AF8", "Mary" = "#69b3a2")) + 
-    theme_ipsum() +
+    ggtitle("Número de bebês chamados Anna e Mary ao longo dos anos") +
     ylab("número de bebês") +
     xlab("anos") +
-    ggtitle("Número de bebês chamados Anna e Mary ao longo dos anos") +
-    guides(fill = guide_legend(title = NULL))
+    guides(fill = guide_legend(title = NULL)) +
+    theme_ipsum() 
