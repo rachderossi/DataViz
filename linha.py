5c89fc6bd7641d6005f22688d44b02328e048abe
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-23 20:18:10 +0100

    Update linha.py

--- linha.py
+++ linha.py
@@ -1,16 +1,23 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Substitua "caminho/para/seu/arquivo.csv" pelo caminho do arquivo CSV em sua máquina
+# data = pd.read_csv("caminho/para/seu/arquivo.csv")
+
+# Lê os dados do arquivo CSV fornecido pela URL e os carrega em um DataFrame chamado 'data'
 data = pd.read_csv(
     "https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/3_TwoNumOrdered.csv", 
     delim_whitespace=True
 )
 
+# Converte a coluna 'date' para o formato de data e hora
 data["date"] = pd.to_datetime(data["date"])
 
+# Extrai as colunas 'date' e 'value' do DataFrame para facilitar a visualização
 date = data["date"]
 value = data["value"]
 
+# Cria o gráfico de linha
 plt.figure(figsize=(10, 6))
 plt.plot(date, value, color="#69b3a2", alpha=0.5, zorder=1)
 plt.title("Evolução do preço do Bitcoin")
