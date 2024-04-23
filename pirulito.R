5f7734a48d73da29654b067b7182cf2d875c7d7d
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-23 18:53:23 +0100

    Update pirulito.R

--- pirulito.R
+++ pirulito.R
@@ -1,6 +1,10 @@
 library(dplyr)
 library(ggplot2)
 library(hrbrthemes)
+library(readr)
+
+# Substitua "caminho/para/seu/arquivo.csv" pelo caminho do arquivo CSV em sua máquina
+# data_freq <- read.csv("caminho/para/seu/arquivo.csv", header = TRUE)
 
 # Contar a frequência de cada classe de veículo
 freq_class <- table(mpg$class)
@@ -11,6 +15,7 @@ data_freq <- data.frame(class = names(freq_class), freq = as.numeric(freq_class)
 # Ordenar o data frame pela frequência em ordem ascendente
 data_freq <- arrange(data_freq, freq)
 
+# Cria o gráfico de pirulito
 data_freq %>%
   ggplot(aes(x = reorder(class, freq), y = freq)) +
   geom_segment(aes(xend = reorder(class, freq), yend = 0), color = "black") +
