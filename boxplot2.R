270947eafa987c6f68aff6417f80471a3e275c6b
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-23 17:46:07 +0100

    Update boxplot2.R

--- boxplot2.R
+++ boxplot2.R
@@ -2,11 +2,15 @@ library(dplyr)
 library(ggplot2)
 library(hrbrthemes)
 
-diamonds %>%
-ggplot(aes(x = cut, y = price, fill = color)) +
+# Substitua "caminho/para/seu/arquivo.csv" pelo caminho do arquivo CSV em sua máquina
+# diamonds <- read.csv("caminho/para/seu/arquivo.csv", header = TRUE)
+
+# Criar o gráfico de boxplot
+  diamonds %>%
+    ggplot(aes(x = cut, y = price, fill = color)) +
     geom_boxplot() +
+    ggtitle("Distribuição do preço dos diamantes por corte e cor") +
     xlab("corte") +
     ylab("preço") +
     labs(fill = "cor") +
-    ggtitle("Distribuição do preço dos diamantes por corte e cor") +
     theme_ipsum()
