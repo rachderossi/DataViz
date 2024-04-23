202c98be4df5dc366b75a1e7a4222039b5d2bba4
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-23 20:14:40 +0100

    Update histograma2.py

--- histograma2.py
+++ histograma2.py
@@ -2,10 +2,16 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
 
+# Substitua "caminho/para/seu/arquivo.csv" pelo caminho do arquivo CSV em sua máquina
+# carseats = pd.read_csv("caminho/para/seu/arquivo.csv")
+
+# Carrega o conjunto de dados 'Carseats' da biblioteca ISLR e carrega os dados
 carseats = sm.datasets.get_rdataset('Carseats', 'ISLR').data
 
+# Filtra os dados para incluir apenas os preços de carseat menores que 300
 carseats_filtered = carseats[carseats['Price'] < 300]
 
+# Cria o gráfico de histograma
 plt.figure(figsize=(10, 6))
 sns.histplot(data=carseats_filtered, x='Price', color='#6E9AF8', alpha=0.8, fill=True, label='Preço')
 sns.histplot(data=carseats_filtered, x='CompPrice', color='#009E73', alpha=0.8, fill=True, label='Renda')
