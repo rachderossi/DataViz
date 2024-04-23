c63d14add9f315b0f5b3fc385c4dbb153377e5c4
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-23 17:30:01 +0100

    Update area.R

--- area.R
+++ area.R
@@ -2,11 +2,19 @@ library(dplyr)
 library(ggplot2)
 library(hrbrthemes)
 
+# Substitua "caminho/para/seu/arquivo.csv" pelo caminho do arquivo CSV em sua máquina
+# data <- read.csv("caminho/para/seu/arquivo.csv", header = TRUE)
+
+# Lendo os dados de um arquivo CSV hospedado em um repositório do GitHub
 data <- read.table("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/3_TwoNumOrdered.csv", header=T)
+
+# Convertendo a coluna de datas para o formato de data
+# Isso é importante para que o gráfico reconheça as datas corretamente
 data$date <- as.Date(data$date)
-  
-data %>%
-    ggplot( aes(x=date, y=value)) +
+
+# Criar o gráfico de área
+  data %>%
+    ggplot(aes(x=date, y=value)) +
     geom_area(fill="#69b3a2", alpha=0.5) +
     geom_line(color="#69b3a2") +
     ggtitle("Evolução do preço do Bitcoin") +
