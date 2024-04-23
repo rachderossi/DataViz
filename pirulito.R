bbd9b8f73b9058021807f8aaecf65d6fb8464d09
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-23 19:01:25 +0100

    Update pirulito.R

--- pirulito.R
+++ pirulito.R
@@ -6,13 +6,13 @@ library(readr)
 # Substitua "caminho/para/seu/arquivo.csv" pelo caminho do arquivo CSV em sua máquina
 # data_freq <- read.csv("caminho/para/seu/arquivo.csv", header = TRUE)
 
-# Contar a frequência de cada classe de veículo
+# Conta a frequência de cada classe de veículo
 freq_class <- table(mpg$class)
 
-# Criar um data frame com os dados da frequência
+# Cria um data frame com os dados da frequência
 data_freq <- data.frame(class = names(freq_class), freq = as.numeric(freq_class))
 
-# Ordenar o data frame pela frequência em ordem ascendente
+# Ordena o data frame pela frequência em ordem ascendente
 data_freq <- arrange(data_freq, freq)
 
 # Cria o gráfico de pirulito
