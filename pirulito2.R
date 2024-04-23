af25595106e07f7893cf173add897591b9d69628
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-23 18:55:17 +0100

    Update pirulito2.R

--- pirulito2.R
+++ pirulito2.R
@@ -1,9 +1,15 @@
 library(dplyr)
 library(ggplot2)
 library(hrbrthemes)
+library(readr)
 
+# Substitua "caminho/para/seu/arquivo.csv" pelo caminho do arquivo CSV em sua máquina
+# data <- read.csv("caminho/para/seu/arquivo.csv", header = TRUE)
+
+# Lendo os dados de um arquivo CSV hospedado em um repositório do GitHub
  data <- read.table("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/7_OneCatOneNum.csv", header=TRUE, sep=",")
-  
+
+# Cria o gráfico de pirulito
   data %>%
     filter(!is.na(Value)) %>%
     arrange(Value) %>%
@@ -12,8 +18,6 @@ library(hrbrthemes)
     ggplot( aes(x=Country, y=Value) ) +
     geom_segment( aes(x=Country ,xend=Country, y=0, yend=Value), color="grey") +
     geom_point(size=3, color="#69b3a2") +
-    coord_flip() +
-    theme_ipsum() +
     theme(
       panel.grid.minor.y = element_blank(),
       panel.grid.major.y = element_blank(),
@@ -21,4 +25,6 @@ library(hrbrthemes)
       plot.title = element_text(hjust = 0.5, size = 15)) +
     ggtitle("Quantidade de armas exportadas pelos 20 maiores exportadores em 2017") +
     xlab("países") +
-    ylab("quantidade de armas") 
+    ylab("quantidade de armas") +
+    coord_flip() +
+    theme_ipsum() 
