1f795064c93f36abd11b28c25652ef0dbef0e7ba
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-23 17:37:47 +0100

    Update barras_agrupadas.R

--- barras_agrupadas.R
+++ barras_agrupadas.R
@@ -2,11 +2,15 @@ library(dplyr)
 library(ggplot2)
 library(hrbrthemes)
 
-diamonds %>%
-ggplot(aes(x = cut, fill = color)) +
+# Substitua "caminho/para/seu/arquivo.csv" pelo caminho do arquivo CSV em sua máquina
+# diamonds <- read.csv("caminho/para/seu/arquivo.csv", header = TRUE)
+
+# Criar o gráfico de barras agrupadas
+  diamonds %>%
+    ggplot(aes(x = cut, fill = color)) +
     geom_bar(position = "dodge") +
+    ggtitle("Qualidade do corte de diamante de acordo com a cor") +
     xlab("corte") +
     ylab("frequência") +
     labs(fill = "cor") +
-    ggtitle("Qualidade do corte de diamante de acordo com a cor") +
     theme_ipsum() 
