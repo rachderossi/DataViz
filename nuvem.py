da778657cc08947ea041cf0e78d5807e84818139
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-23 20:23:36 +0100

    Update nuvem.py

--- nuvem.py
+++ nuvem.py
@@ -1,12 +1,14 @@
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
+# Lista de países e suas frequências
 paises = ["Brasil", "Estados Unidos", "Canadá", "Reino Unido", "França", "Alemanha", "Austrália", "Japão", "China", "Índia"]
 frequencias = [50, 30, 40, 35, 25, 20, 45, 15, 10, 5]
 
 # Criar um dicionário com as palavras e suas frequências
 word_freq = {paises[i]: frequencias[i] for i in range(len(paises))}
 
+# Cria a nuvem de palavras
 wordcloud = WordCloud(width=800, height=400, colormap='Dark2').generate_from_frequencies(word_freq)
 
 plt.figure(figsize=(10, 5))
