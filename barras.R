e82789adc0114b7cdd224930ac8e83b3c2f68f7b
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-23 17:31:40 +0100

    Update barras.R

--- barras.R
+++ barras.R
@@ -2,12 +2,16 @@ library(dplyr)
 library(ggplot2)
 library(hrbrthemes)
 
-  mpg %>%
+# Substitua "caminho/para/seu/arquivo.csv" pelo caminho do arquivo CSV em sua máquina
+# mpg <- read.csv("caminho/para/seu/arquivo.csv", header = TRUE)
+
+# Criar o gráfico de barras
+ mpg %>%
     count(class) %>%
     mutate(class = reorder(class, n)) %>%
     ggplot(aes(x = class, y = n)) + 
-    geom_bar(fill = "#6E9AF8", stat = "identity") +  
+    geom_bar(fill = "#6E9AF8", stat = "identity") + 
+    ggtitle("Distribuição das classes de veículo") +
     xlab("classe") +
     ylab("frequência") +
-    ggtitle("Distribuição das classes de veículo") +
     theme_ipsum()
