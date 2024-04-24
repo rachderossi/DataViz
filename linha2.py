140e3a13209440d3fe09939709dba2167a59d220
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-24 22:12:14 +0100

    Update linha2.py

--- linha2.py
+++ linha2.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 # Define a URL do arquivo CSV contendo os dados de nomes de bebê
 url = "https://github.com/hadley/data-baby-names/raw/master/baby-names.csv"
 
+# Caso não seja necessário filtrar seus dados pule para a linha 24
 # Lê os dados do arquivo CSV e os carrega em um DataFrame chamado 'baby_names'
 baby_names = pd.read_csv(url)
 
@@ -21,8 +22,9 @@ filtered_data = baby_names[(baby_names['name'].isin(names)) & (baby_names['sex']
 
 # Criar o gráfico de linha
 plt.figure(figsize=(10, 6))
-for name, color in zip(names, ['#6E9AF8', '#009E73', '#FFA07A']):
+for name, color in zip(names, ['#6E9AF8', '#009E73', '#FFA07A']): # É possível definir cores para cada grupo
     data = filtered_data[filtered_data['name'] == name]
+    # year é a variável x e percent é a variável y
     plt.plot(data['year'], data['percent'], label=name, color=color)
 plt.title('Popularidade de nomes Americanos nos últimos 30 anos')
 plt.xlabel('Ano')
