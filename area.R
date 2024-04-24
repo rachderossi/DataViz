e870a18a86612a0d5e1d4354d442195340012249
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-24 17:42:06 +0100

    Update area.R

--- area.R
+++ area.R
@@ -9,6 +9,7 @@ library(readr)
 # Lendo os dados de um arquivo CSV hospedado em um repositório do GitHub
 data <- read.table("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/3_TwoNumOrdered.csv", header=T)
 
+# Se a sua variável ordenada for uma data é importe converter para o formato correto
 # Convertendo a coluna de datas para o formato de data
 # Isso é importante para que o gráfico reconheça as datas corretamente
 data$date <- as.Date(data$date)
