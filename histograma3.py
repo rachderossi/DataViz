771b19893b5b8e7f96720b8a2d2ae326ad260871
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-24 22:05:45 +0100

    Update histograma3.py

--- histograma3.py
+++ histograma3.py
@@ -4,11 +4,13 @@ import matplotlib.pyplot as plt
 # Substitua "caminho/para/seu/arquivo.csv" pelo caminho do arquivo CSV em sua máquina
 # iris = pd.read_csv("caminho/para/seu/arquivo.csv")
 
+# A linha 5 substitui essa, pois aqui estou utilizando dados de uma base de dados do Python
 # Carrega o conjunto de dados 'iris' fornecido pela seaborn
 iris = sns.load_dataset('iris')
 
 # Criar o gráfico de histograma
 plt.figure(figsize=(10, 6))
+# hue é a variável que agrupa os dados
 sns.histplot(data=iris, x='petal_length', hue='species', binwidth=0.2, alpha=0.5, multiple='stack', palette='Set1')
 plt.xlabel('Comprimento da pétala')
 plt.ylabel('Frequência')
