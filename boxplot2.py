5509840c533b3b361e8f1f30258b13deb3220d55
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-24 20:32:56 +0100

    Update boxplot2.py

--- boxplot2.py
+++ boxplot2.py
@@ -9,6 +9,7 @@ diamonds = sns.load_dataset('diamonds')
 
 # Criar o gráfico de boxplot
 plt.figure(figsize=(10, 6))
+# hue é a variável que agrupa os dados
 sns.boxplot(x='cut', y='price', data=diamonds, hue='color', palette='Set1')
 plt.title('Distribuição do preço dos diamantes por corte')
 plt.xlabel('Corte')
