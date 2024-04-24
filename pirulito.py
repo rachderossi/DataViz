a93429d94ee29cf6c2a2fbd432158ce1a538e6ae
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-24 22:16:02 +0100

    Update pirulito.py

--- pirulito.py
+++ pirulito.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 # Substitua "caminho/para/seu/arquivo.csv" pelo caminho do arquivo CSV em sua máquina
 # data = pd.read_csv("caminho/para/seu/arquivo.csv")
 
+# A linha 5 substitui essa, pois aqui estou criando dados fictícios
 # Cria um dicionário com os dados das classes de veículo
 data = {
     'class': ['compact', 'midsize', 'suv', 'midsize', 'compact', 'suv', 'suv', 'compact'],
@@ -12,6 +13,7 @@ data = {
 # Cria um DataFrame a partir do dicionário
 mpg = pd.DataFrame(data)
 
+# Caso você já tenha a variável de frequência calculada pode pular para a linha 24
 # Conta as ocorrências de cada classe de veículo e armazena em um novo DataFrame
 class_counts = mpg['class'].value_counts().reset_index()
 
@@ -24,6 +26,7 @@ class_counts = class_counts.sort_values(by='freq')
 # Criar o gráfico de pirulito
 plt.figure(figsize=(10, 6))
 plt.hlines(y=class_counts['class'], xmin=0, xmax=class_counts['freq'], color='black')
+# class é a variável x e freq é a variável y
 plt.plot(class_counts['freq'], class_counts['class'], 'o', color='#6E9AF8', markersize=8)
 plt.xlabel('Frequência')
 plt.ylabel('Classe')
