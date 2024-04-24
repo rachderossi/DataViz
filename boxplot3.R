4df43f5691b90fba1d833476fa1beee40b20ec2f
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-24 21:06:23 +0100

    Update boxplot3.R

--- boxplot3.R
+++ boxplot3.R
@@ -8,7 +8,7 @@ library(readr)
 
 # Criar o gráfico de boxplot
   mtcars %>%
-    # a variável x está sendo convertida para fator, pois "cyl" é uma variável numérica
+    # A variável x está sendo convertida para fator, pois "cyl" é uma variável numérica
     # fill é a variável que agrupa os dados
     # labels define o nome de cada grupo, é possível ser customizado pela quantidade de grupos 
     ggplot(aes(x = as.factor(cyl), y = mpg, fill = factor(am, labels = c("Automático", "Manual")))) +
