3e8b52a569ac53f0d6506668e759126da2c90c18
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-24 21:12:10 +0100

    Update densidade2.R

--- densidade2.R
+++ densidade2.R
@@ -7,12 +7,13 @@ library(readr)
 # Substitua "caminho/para/seu/arquivo.csv" pelo caminho do arquivo CSV em sua máquina
 # data <- read.csv("caminho/para/seu/arquivo.csv", header = TRUE)
 
+A linha 8 substitui essa, pois aqui estou utilizando dados de uma base de dados do R
 # Lendo o conjunto de dados Carseats do pacote ISLR
 data <- ISLR::Carseats
   
 # Criar o gráfico de densidade
 data %>%
-    filter(Price < 300) %>%
+    filter(Price < 300) %>% # Necessário apenas se quiser filtrar valores específicos
     ggplot() +
     geom_density(aes(x = Price, fill = "preço"), alpha = 0.8) +  
     geom_density(aes(x = CompPrice, fill = "renda"), alpha = 0.8) +  
