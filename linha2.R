6f4c4c39638c2c1c61240a0b376760c6d2d054ab
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-24 21:37:04 +0100

    Update linha2.R

--- linha2.R
+++ linha2.R
@@ -8,15 +8,17 @@ library(readr)
 # name <- read.csv("caminho/para/seu/arquivo.csv", header = TRUE)
 
 # Filtrando o conjunto de dados babynames pelo nome e sexo de interesse
+# Caso não seja necessário filtrar seus dados pule para a linha 17
   name <- babynames %>%
     filter(name %in% c("Ashley", "Patricia", "Helen")) %>%
     filter(sex == "F")
 
 # Criar o gráfico de linha
   name %>%
+    # Caso exista, substitua n pela sua variável de frequência, group é a variável que agrupa os dados
     ggplot(aes(x = year, y = n, group = name, color = name)) +
     geom_line() +
-    scale_color_manual(values = c("#6E9AF8", "#009E73", "#FFA07A"), name = NULL) +
+    scale_color_manual(values = c("#6E9AF8", "#009E73", "#FFA07A"), name = NULL) + # É possível definir cores para cada grupo
     ggtitle("Popularidade de nomes Americanos nos últimos 30 Anos") +
     xlab("ano") +
     ylab("número de bebês") +
