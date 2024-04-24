abecdf6dc89815ba2526aa4ee1c34d457d833b72
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-24 21:59:42 +0100

    Update densidade3.py

--- densidade3.py
+++ densidade3.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 # Substitua "caminho/para/seu/arquivo.csv" pelo caminho do arquivo CSV em sua máquina
 # iris = pd.read_csv("caminho/para/seu/arquivo.csv")
 
+# A linha 5 substitui essa, pois aqui estou utilizando dados de uma base de dados do Python
 # Carrega o conjunto de dados 'iris' fornecido pela seaborn
 iris = sns.load_dataset('iris')
 
