67cd321619f1f6f9128121bfba53b81851c69d19
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-24 21:33:30 +0100

    Update histograma2.R

--- histograma2.R
+++ histograma2.R
@@ -7,16 +7,17 @@ library(readr)
 # Substitua "caminho/para/seu/arquivo.csv" pelo caminho do arquivo CSV em sua máquina
 # data <- read.csv("caminho/para/seu/arquivo.csv", header = TRUE)
 
+# A linha 8 substitui essa, pois aqui estou utilizando dados de uma base de dados do R
 # Lendo o conjunto de dados Carseats do pacote ISLR
   data <- ISLR::Carseats
 
 # Criar o gráfico de histograma
   data %>%
-    filter(Price < 300) %>%
+    filter(Price < 300) %>% # Necessário apenas se quiser filtrar valores específicos
     ggplot() +
     geom_histogram(aes(x = Price, fill = "preço"), alpha = 0.8) +  
     geom_histogram(aes(x = CompPrice, fill = "renda"),  alpha = 0.8) +  
-    scale_fill_manual(values = c("#6E9AF8", "#009E73"), name= NULL) +  
+    scale_fill_manual(values = c("#6E9AF8", "#009E73"), name= NULL) + # É possível definir cores para cada grupo
     ggtitle("Distribuição de preço e renda") +
     xlab("valor") +
     ylab("frequência") +
