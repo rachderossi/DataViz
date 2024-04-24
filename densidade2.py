0ad60659278259500881a61ab4bbbb7b8f9e379e
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-24 21:17:35 +0100

    Update densidade2.py

--- densidade2.py
+++ densidade2.py
@@ -5,10 +5,12 @@ import statsmodels.api as sm
 # Substitua "caminho/para/seu/arquivo.csv" pelo caminho do arquivo CSV em sua máquina
 # carseats = pd.read_csv("caminho/para/seu/arquivo.csv")
 
+# A linha 6 substitui essa, pois aqui estou utilizando dados de uma base de dados do Python
 # Carrega o conjunto de dados 'Carseats' da biblioteca ISLR e carrega os dados
 carseats = sm.datasets.get_rdataset('Carseats', 'ISLR').data
 
 # Filtra os dados para incluir apenas os preços de carseat menores que 300
+# Necessário apenas se quiser filtrar valores específicos
 carseats_filtered = carseats[carseats['Price'] < 300]
 
 # Criar o gráfico de densidade
