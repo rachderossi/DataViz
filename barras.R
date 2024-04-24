950c63acd71cdb33d73f1975d6e1615523ea8601
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-24 21:04:47 +0100

    Update barras.R

--- barras.R
+++ barras.R
@@ -8,9 +8,9 @@ library(readr)
 
 # Criar o gráfico de barras
  mpg %>%
-    count(class) %>% # substitua class pela sua variável categórica
-    # caso exista, substitua n pela sua variável de frequência
-    mutate(class = reorder(class, n)) %>% # ordenando a variável pela frequência
+    count(class) %>% # Substitua class pela sua variável categórica
+    # Caso exista, substitua n pela sua variável de frequência
+    mutate(class = reorder(class, n)) %>% # Ordenando a variável pela frequência
     ggplot(aes(x = class, y = n)) + 
     geom_bar(fill = "#6E9AF8", stat = "identity") + 
     ggtitle("Distribuição das classes de veículo") +
