72b523728d9f93421c51043b48b7bbdcc16d7ae6
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-24 21:26:40 +0100

    Update dispersao.py

--- dispersao.py
+++ dispersao.py
@@ -5,6 +5,7 @@ import statsmodels.api as sm
 # Substitua "caminho/para/seu/arquivo.csv" pelo caminho do arquivo CSV em sua máquina
 # carseats = pd.read_csv("caminho/para/seu/arquivo.csv")
 
+# A linha 6 substitui essa, pois aqui estou utilizando dados de uma base de dados do Python
 # Carrega o conjunto de dados 'Carseats' da biblioteca ISLR e carrega os dados
 carseats = sm.datasets.get_rdataset('Carseats', 'ISLR').data
 
