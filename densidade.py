4755ce49c3b2674ab1d1dd0c63b3eb84e199b794
Raquel Rossi <raquelderossi@hotmail.com>
2024-05-30 17:31:33 +0100

    Update densidade.py

--- densidade.py
+++ densidade.py
@@ -18,8 +18,8 @@ data_filtered = data[data['price'] < 300]
 # Criar o gráfico de densidade
 plt.figure(figsize=(10, 6))
 sns.kdeplot(data=data_filtered['price'], fill=True, color='#69b3a2', alpha=0.8)
-plt.title("Distribuição de preços de aluguel")
-plt.xlabel("Preço")
+plt.title("Preço por diária de apartamentos Airbnb na Riviera Francesa")
+plt.xlabel("Preço (euro)")
 plt.ylabel("Densidade")
 plt.grid(True)
 plt.show()
