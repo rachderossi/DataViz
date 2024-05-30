aabc99833d1909b5a96854fb1a7f669648bfe7e6
Raquel Rossi <raquelderossi@hotmail.com>
2024-05-30 17:58:39 +0100

    Update linha2.R

--- linha2.R
+++ linha2.R
@@ -19,8 +19,14 @@ library(readr)
     ggplot(aes(x = year, y = n, group = name, color = name)) +
     geom_line() +
     scale_color_manual(values = c("#6E9AF8", "#009E73", "#FFA07A"), name = NULL) + # É possível definir cores para cada grupo
-    ggtitle("Popularidade de nomes Americanos nos últimos 30 Anos") +
+    ggtitle("Popularidade dos nomes Ashley, Patrícia e Helen
+                 nos EUA entre (1880-2020) ") +
     xlab("ano") +
     ylab("número de bebês") +
     guides(color = guide_legend(title.position = "left", title.hjust = 1, title = NULL)) +
-    theme_ipsum() 
+    theme_ipsum() +
+    theme(axis.text.x = element_text(size = 14),
+          axis.text.y = element_text(size = 14),
+          axis.title.x = element_text(size = 14),
+          axis.title.y = element_text(size = 14),
+          legend.text = element_text(size = 14))
