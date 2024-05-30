b72d90fa7e75538d3bce2c66648797213cc9b206
Raquel Rossi <raquelderossi@hotmail.com>
2024-05-30 17:56:00 +0100

    Update histograma3.R

--- histograma3.R
+++ histograma3.R
@@ -11,8 +11,14 @@ library(readr)
       # fill é a variável que agrupa os dados
       ggplot(aes(x = Petal.Length, fill = Species)) +
       geom_histogram(binwidth = 0.2, alpha = 0.5, position = "identity") +
-      ggtitle("Distribuição do comprimento da sépala por espécie de flor") +
-      xlab("comprimento da pétala") +
+      ggtitle("Distribuição do comprimento da pétala por espécie de flor") +
+      xlab("comprimento da pétala (cm)") +
       ylab("frequência") +
       labs(fill = "espécies") +
-      theme_ipsum()
+      theme_ipsum() +
+      theme(axis.text.x = element_text(size = 14),
+            axis.text.y = element_text(size = 14),
+            axis.title.x = element_text(size = 14),
+            axis.title.y = element_text(size = 14),
+            legend.title = element_text(size = 14),
+            legend.text = element_text(size = 14))
