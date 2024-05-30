f71a1527697a56a504217df8e0bf52905646f80f
Raquel Rossi <raquelderossi@hotmail.com>
2024-05-30 17:55:12 +0100

    Update histograma2.py

--- histograma2.py
+++ histograma2.py
@@ -17,8 +17,9 @@ carseats_filtered = carseats[carseats['Price'] < 300]
 plt.figure(figsize=(10, 6))
 sns.histplot(data=carseats_filtered, x='Price', color='#6E9AF8', alpha=0.8, fill=True, label='Preço')
 sns.histplot(data=carseats_filtered, x='CompPrice', color='#009E73', alpha=0.8, fill=True, label='Renda')
-plt.title('Distribuição de preço e renda')
-plt.xlabel('Valor')
+plt.title('Distribuição conjunta dos preços dos concorrentes e da 
+    empresa para assentos de carro em diferentes locais')
+plt.xlabel('Preço (dólar)')
 plt.ylabel('Frequência')
 plt.legend(title=None)
 plt.tight_layout()
